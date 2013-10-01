c6f3612b1cd9bc0cc9e9cbd2bb0b00b80a51e6d6
Ricky Dunlop <ricky@rehabstudio.com>
2013-10-01 16:44:36 +0100

    Initital commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('djsreaderApp')
+  .controller('MainCtrl', function($scope, $http, $timeout, $filter) {
+    function storyInCollection(story) {
+      for (var i = 0; i < $scope.stories.length; i++) {
+        if ($scope.stories[i].id === story.id) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function addStories(stories) {
+      var changed = false;
+      angular.forEach(stories, function(story, key) {
+        if (!storyInCollection(story)) {
+          $scope.stories.push(story);
+          changed = true;
+        }
+      });
+
+      if (changed) {
+        $scope.stories = $filter('orderBy')($scope.stories, 'date');
+      }
+    }
+
+    $scope.refreshInterval = 60;
+    $scope.feeds = [{
+      url: 'http://dailyjs.com/atom.xml'
+    }];
+    $scope.stories = [];
+
+    $scope.addFeed = function(feed) {
+      if (feed.$valid) {
+        // Copy this feed instance and reset the URL in the form
+        var newFeed = angular.copy(feed);
+        $scope.feeds.push(feed);
+        $scope.fetchFeed(feed);
+        $scope.newFeed.url = '';
+      }
+    };
+
+    $scope.fetchFeed = function(feed) {
+      feed.items = [];
+
+      var apiUrl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'";
+      apiUrl += encodeURIComponent(feed.url);
+      apiUrl += "'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+      $http.jsonp(apiUrl).
+        success(function(data, status, headers, config) {
+          if (data.query.results) {
+            feed.items = data.query.results.entry;
+          }
+          addStories(feed.items);
+        }).
+        error(function(data, status, headers, config) {
+          console.error('Error fetching feed:', data);
+        });
+
+      $timeout(function() { $scope.fetchFeed(feed); }, $scope.refreshInterval * 1000);
+    };
+
+    $scope.deleteFeed = function(feed) {
+      $scope.feeds.splice($scope.feeds.indexOf(feed), 1);
+    };
+
+    $scope.fetchFeed($scope.feeds[0]);
+  });
